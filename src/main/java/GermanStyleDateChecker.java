f52d0cb4b039ab484f24e803de2ab6542d1ac579
Thomas Antensteiner <thomas.antensteiner@hotmail.com>
2018-05-16 20:20:57 +0200

    Abgabe

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -3,6 +3,7 @@ import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
     public static Matcher getMatcher(String dateString) {
-        return null;
+       Pattern p = Pattern.compile("((^3(0|1))|(^[0-2]{0,1}[0-9]))(\\.|-| )((0{0,1}[1-9])|(1[0-2]))(\\.|-| )(((19|20)[0-9]{2}$)|[0-9]{2}$)");
+        return p.matcher(dateString);
     }
 }
